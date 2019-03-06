c3431b410c50071922923176bfe8558a1c360bc4
Kristofer Monisit <kristofer.monisit@analog.com>
2019-03-06 12:25:47 +0800

    Force local installation

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -e
 umask 022
 if test -n "$HOMEBREW_FORCE_VENDOR_RUBY" || ! command -v ruby >/dev/null; then
-	eval "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install-ruby`"
+	eval "`curl -fsSL https://raw.githubusercontent.com/kitmonisit/install/master/install-ruby`"
 fi
-exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install`" "$@"
+exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/kitmonisit/install/master/install`" "$@"
